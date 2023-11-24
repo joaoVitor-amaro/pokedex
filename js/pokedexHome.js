7d7eaddc2df8539135fb5672f4153190d2510ee8
João Vitor Amaro de Melo <106560064+joaoVitor-amaro@users.noreply.github.com>
2023-11-23 21:00:12 -0300

    feat: adicionado a função de criar os card dos pokémons

--- js/pokedexHome.js
+++ js/pokedexHome.js
@@ -1,3 +1,5 @@
+const sectionListpoke = document.querySelector("#ListPoke")
+
 const getPokemom = async () => {
     const response = await fetch("https://pokeapi.co/api/v2/pokemon?limit=25&offset=$0")
     const data = await response.json()
@@ -9,3 +11,26 @@ const getPokemom = async () => {
     console.log(pokeData)
     return pokeData
 }
+
+const creatListPoke = async () => {
+    const data = await getPokemom()
+    data.forEach((pokemom) => {
+        const articleCardPokemom = document.createElement("article")
+        const divIdPoke = document.createElement('div')
+        const imagePoke = document.createElement('img')
+        const titleNamePoke = document.createElement('h2')
+        articleCardPokemom.setAttribute('id', 'cardPoke')
+        divIdPoke.setAttribute("id", 'idPoke')
+        imagePoke.setAttribute('id', 'imgPoke')
+        titleNamePoke.setAttribute('id', 'titleNamePoke')
+        divIdPoke.innerHTML = `N° ${pokemom.id}`
+        imagePoke.src = pokemom.sprites.front_default
+        titleNamePoke.innerHTML = pokemom.name
+        articleCardPokemom.appendChild(divIdPoke)
+        articleCardPokemom.appendChild(imagePoke)
+        articleCardPokemom.appendChild(titleNamePoke)
+        sectionListpoke.appendChild(articleCardPokemom)
+    })
+}
+
+creatListPoke()
