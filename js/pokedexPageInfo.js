2a509dae8d7eb269af1fd1d84caf27a4e76b2727
João Vitor Amaro de Melo <106560064+joaoVitor-amaro@users.noreply.github.com>
2023-11-24 10:32:08 -0300

    feat: Criado a função de pegar os dados do pokémon pelo id da url

--- js/pokedexPageInfo.js
+++ js/pokedexPageInfo.js
@@ -2,3 +2,9 @@ const getidPokePagina = () => {
     const urlSearchParam = new URLSearchParams(window.location.search)
     return urlSearchParam.get("id")
 }
+
+const GetInforPoke = async (id) =>{
+    const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}/`)
+    const date = await res.json()
+    return date
+}
