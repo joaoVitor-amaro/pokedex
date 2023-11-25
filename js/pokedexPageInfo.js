30982c537a1b554a06e85d5270b3ae0fce2cc7a3
João Vitor Amaro de Melo <106560064+joaoVitor-amaro@users.noreply.github.com>
2023-11-25 09:19:54 -0300

    feat: criado a função para buscar as variações do pokémon

--- js/pokedexPageInfo.js
+++ js/pokedexPageInfo.js
@@ -3,6 +3,7 @@ const containerImgPokeInfor = document.querySelector('#containerImgPokeInfor')
 const heightPoke = document.querySelector("#heightPoke")
 const weightPoke = document.querySelector("#weightPoke")
 const typesPoke = document.querySelector('#typesPoke')
+const variacaoPokemon = document.querySelector("#varicaoPokemom")
 
 const getidPokePagina = () => {
     const urlSearchParam = new URLSearchParams(window.location.search)
@@ -26,6 +27,24 @@ const typesPokemon = (typesPoke) => {
     return spanType
 }
 
+const ImgvariacaoPoke = (pokeVariacao, namePokemom) => {
+    const listVariacaoPoke = ["front_default", "front_female", "front_shiny","front_shiny_female"]
+    const imgFigurePokeVaria = document.createElement('figure')
+    imgFigurePokeVaria.setAttribute('id', "containerVariacaoPoke")
+    for (var valoresVaricao of listVariacaoPoke) {
+            if (pokeVariacao[valoresVaricao] != null) {
+                const imgVariacaoPoke = document.createElement('img')
+                imgVariacaoPoke.src = pokeVariacao[valoresVaricao]
+                imgVariacaoPoke.alt = namePokemom
+                imgVariacaoPoke.setAttribute("id", "pokemomvariacao")
+                imgVariacaoPoke.setAttribute("width", "180")
+                imgVariacaoPoke.setAttribute('height', '180')
+                imgFigurePokeVaria.appendChild(imgVariacaoPoke)
+            }
+    }
+    return imgFigurePokeVaria
+}
+
 const infoPoke = (id) => {
     GetInforPoke(id)
         .then((pokemom) => {
@@ -40,6 +59,7 @@ const infoPoke = (id) => {
             imgPokeInfo.setAttribute('id', "imagePoke")
             containerImgPokeInfor.appendChild(imgPokeInfo)
             typesPoke.appendChild(typesPokemon(pokemom.types))
+            variacaoPokemon.appendChild(ImgvariacaoPoke(pokemom.sprites))
         })
 }
 
